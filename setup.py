771200e3b508b0d44ec641a50a204c70c2535758
ryuvi <vicenters10@gmail.com>
2023-06-19 17:50:46 -0300

    more base files added

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from re import search
 
 setup(name='bulk_renamer',
       version='1.0',
@@ -9,6 +8,6 @@ setup(name='bulk_renamer',
       author=['ryuvi', 'HPereiraVitor'],
       author_email=['vicenters10@gmail.com'],
       url='',
-      packages=[search(r'\w+(?=\=)', package).group() for package in open('requirements.txt', 'r').readlines()],
+      packages=['black', 'tk'],
       package_dir={'bulk_renamer': 'lib'}
      )
